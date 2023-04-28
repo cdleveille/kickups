9cd0db1cfa12bdcdfe78b82abc59b5778f824444
Chris Leveille <cdleveille@gmail.com>
2023-04-28 18:52:37 -0400

    fix broken build

--- src/server/helpers/socket.ts
+++ src/server/helpers/socket.ts
@@ -2,7 +2,7 @@ import { Server as HttpServer } from "http";
 import { Server, Socket } from "socket.io";
 
 import { newScore, sendTopScoresToClient } from "@helpers";
-import { IScore, SocketEvent } from "@shared";
+import { IEncryptedScore, SocketEvent } from "@shared";
 
 export const initSocket = (httpServer: HttpServer) => {
 	const io = new Server(httpServer);
@@ -11,7 +11,7 @@ export const initSocket = (httpServer: HttpServer) => {
 			await sendTopScoresToClient(socket);
 		});
 
-		socket.on(SocketEvent.CLIENT_SEND_NEW_SCORE, async (score: IScore) => {
+		socket.on(SocketEvent.CLIENT_SEND_NEW_SCORE, async (score: IEncryptedScore) => {
 			await newScore(score, socket);
 		});
 	});
